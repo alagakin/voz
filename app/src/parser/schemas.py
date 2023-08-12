3a7b8b4f7d7d3b0377c3846a156e90140913413a
alex <agakinalexandr@gmail.com>
2023-08-12 10:43:42 +0200

    setting routes

--- app/src/parser/schemas.py
+++ app/src/parser/schemas.py
@@ -1,6 +1,7 @@
+import hashlib
 from typing import List
 
-from pydantic import BaseModel, validator, Field
+from pydantic import BaseModel, validator, Field, root_validator
 from datetime import datetime, date
 
 
@@ -8,14 +9,15 @@ from datetime import datetime, date
 class TrainSchema(BaseModel):
     id: int = 1
     number: int
-    arrive: datetime
+    # todo: how to get know of day of the date?
+    arrival: datetime
     departure: datetime
     rang: str
     date: datetime = Field(default_factory=datetime.now)
     station_from: int
     station_to: int
 
-    @validator("arrive", "departure", pre=True)
+    @validator("arrival", "departure", pre=True)
     def parse_datetime(cls, value):
         if isinstance(value, datetime):
             return value
@@ -34,11 +36,18 @@ class RouteStationSchema(BaseModel):
     name: str
     name1: str
     time: int
-    arrive: datetime
+    arrival: datetime
     departure: datetime
-    date: datetime = Field(default_factory=datetime.now)
+    date: datetime = Field(default_factory=datetime.now().date)
 
-    @validator("arrive", "departure", pre=True)
+    @root_validator
+    def strip_strings(cls, values):
+        for field, value in values.items():
+            if isinstance(value, str):
+                values[field] = value.strip()
+        return values
+
+    @validator("arrival", "departure", pre=True)
     def parse_datetime(cls, value):
         if isinstance(value, datetime):
             return value
@@ -52,7 +61,30 @@ class RouteStationSchema(BaseModel):
 
 
 class RouteSchema(BaseModel):
+    id: str = ""
     train_id: int
     train_number: int
+    first_station: str = ''
+    last_station: str = ''
     stations: List[RouteStationSchema]
+
+    @root_validator
+    def set_first_and_last(cls, values):
+        stations = values.get('stations')
+        if len(stations) < 2:
+            return values
+
+        values["first_station"] = stations[0].name
+        values["last_station"] = stations[len(stations) - 1].name
+
+        return values
+
+    @root_validator
+    def calc_hash(cls, values):
+        fields = {field: values.get(field) for field in values if field != "id"}
+        json = str(fields)
+        hash_object = hashlib.sha256(json.encode())
+        hash_value = hash_object.hexdigest()
+        values['id'] = hash_value
+        return values
 
--- app/src/parser/services.py
+++ app/src/parser/services.py
@@ -2,8 +2,10 @@ import json
 from typing import List
 import aiohttp
 from bs4 import BeautifulSoup
-
-from src.parser.schemas import TrainSchema, RouteStationSchema, RouteSchema
+from config import MONGO_DB
+from database import get_client
+from log import logger
+from parser.schemas import TrainSchema, RouteStationSchema, RouteSchema
 
 
 async def fetch_url(url) -> str:
@@ -22,16 +24,19 @@ async def get_list_of_trains(url) -> List[TrainSchema]:
     res = []
     # todo errors
     for i in range(1, len(rows)):
-        row = rows[i]
-        item = {}
-        cells = row.find_all('td')
-        item["number"] = int(cells[0].text.strip())
-        item["arrive"] = cells[1].text.strip()
-        item["departure"] = cells[3].text.strip()
-        item["rang"] = cells[4].find('img').get('title')
-        item["id"] = cells[8].find('button').get('data-idvoza')
-        item["station_from"] = cells[8].find('button').get('data-stod')
-        item["station_to"] = cells[8].find('button').get('data-stdo')
+        try:
+            row = rows[i]
+            item = {}
+            cells = row.find_all('td')
+            item["number"] = int(cells[0].text.strip())
+            item["arrival"] = cells[1].text.strip()
+            item["departure"] = cells[3].text.strip()
+            item["rang"] = cells[4].find('img').get('title')
+            item["id"] = cells[8].find('button').get('data-idvoza')
+            item["station_from"] = cells[8].find('button').get('data-stod')
+            item["station_to"] = cells[8].find('button').get('data-stdo')
+        except Exception:
+            logger.error(str(row))
 
         res.append(TrainSchema(**item))
 
@@ -50,16 +55,63 @@ async def get_routes(train: TrainSchema) -> RouteSchema:
     stations = []
     for station in data['stanicavoza']:
         item = {}
-        item['arrive'] = station["DOLAZAK"]
-        item["departure"] = station["POLAZAK"]
-        item["number"] = station["RBSTANICE"]
-        item["name"] = station["NAZIV"]
-        item["name1"] = station["NAZIV1"]
-        item["time"] = station["vremevoznje"]
-        item["id"] = station["Sifra"]
-        stations.append(RouteStationSchema(**item))
+        try:
+            item['arrival'] = station["DOLAZAK"]
+            item["departure"] = station["POLAZAK"]
+            item["number"] = station["RBSTANICE"]
+            item["name"] = station["NAZIV"]
+            item["name1"] = station["NAZIV1"]
+            item["time"] = station["vremevoznje"]
+            item["id"] = station["Sifra"]
+            stations.append(RouteStationSchema(**item))
+        except Exception:
+            logger.error(json.loads(station))
+
     train_id = data["IDVOZA"]
     train_number = data["BROJVOZA"]
     res = RouteSchema(stations=stations, train_id=train_id, train_number=train_number)
 
     return res
+
+
+async def set_routes():
+    stations = await get_stations()
+    stations = stations[:1]
+    for station in stations:
+        trains = await get_list_of_trains(
+            f"https://w3.srbvoz.rs/redvoznje//stanicni/{station['name']}/{station['id']}/11.08.2023/0000/polazak/999/sr")
+
+        routes = []
+
+        for train in trains:
+            routes.append(await get_routes(train))
+
+        client = await get_client()
+        db = client[MONGO_DB]
+        collection = db["routes"]
+
+        for route in routes:
+            route = route.dict()
+            for station in route["stations"]:
+                station["arrival"] = station["arrival"].isoformat()
+                station["departure"] = station["departure"].isoformat()
+                station["date"] = station["date"].isoformat()
+
+            query = {"id": route["id"]}
+            existing_document = await collection.find_one(query)
+
+            if not existing_document:
+                await collection.insert_one(route)
+
+
+async def get_stations():
+    client = await get_client()
+    db = client[MONGO_DB]
+    collection = db["stations"]
+    stations = []
+    async for document in collection.find():
+        stations.append(document)
+
+    client.close()
+
+    return stations
