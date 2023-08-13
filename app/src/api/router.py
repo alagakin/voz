984a7d90efbcfad6bc4a8cb4caee924ae6c3ec8d
alex <agakinalexandr@gmail.com>
2023-08-13 14:01:27 +0200

    simple route searcher

---
+++ app/src/api/router.py
@@ -0,0 +1,40 @@
+from fastapi import APIRouter
+
+from config import MONGO_DB
+from database import get_client
+
+router = APIRouter(
+    prefix="/api/v1",
+)
+
+
+@router.get('/find-routes/')
+async def index(station_from, station_to):
+    client = await get_client()
+    db = client[MONGO_DB]
+    collection = db["routes"]
+
+    routes = []
+    async for route in collection.find({}):
+        if route_matches(route, station_from, station_to):
+            route["_id"] = str(route["_id"])
+            routes.append(route)
+    return routes
+
+
+def route_matches(route, station_from, station_to) -> bool:
+    station_from_key = -1
+    station_to_key = -1
+    for key, station in enumerate(route['stations']):
+        if station['name'] == station_from:
+            station_from_key = key
+        if station['name'] == station_to:
+            station_to_key = key
+
+    if station_from_key == -1 or station_to_key == -1:
+        return False
+
+    if station_from_key >= station_to_key:
+        return False
+
+    return True
