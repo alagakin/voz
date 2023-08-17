c75856a6e60cad637c9514fb17021eb52c405403
alex <agakinalexandr@gmail.com>
2023-08-17 22:06:03 +0200

    parsing routes with celery

--- app/src/celery_config.py
+++ app/src/celery_config.py
@@ -3,5 +3,5 @@ from celery import Celery
 app = Celery(
     'tasks',  # Name of the app
     broker='redis://redis:6379/0',  # URL to the Redis broker
-    backend='rpc://',  # Use RPC backend for results
+    include=["parser.services"]
 )
