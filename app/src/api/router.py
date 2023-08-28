6f616b7527880c61191de4799557ecfe4056ee9e
alex <agakinalexandr@gmail.com>
2023-08-28 10:28:55 +0200

    removed old routes searcher

--- app/src/api/router.py
+++ app/src/api/router.py
@@ -1,83 +1,14 @@
-from typing import Dict
 import pytz
 from fastapi import APIRouter, Depends
-from starlette.status import HTTP_400_BAD_REQUEST
-
 from config import MONGO_DB
 from database import get_async_client
-from datetime import datetime
 from meili import get_client as get_search_client, get_index
-from fastapi.exceptions import HTTPException
 
 timezone = pytz.timezone('Europe/Belgrade')
 
 router = APIRouter(
     prefix="/api/v1",
 )
-
-
-@router.get('/find-routes/')
-async def index(station_from: int, station_to: int, date: str = None):
-    try:
-        if date is None:
-            date = datetime.now(tz=timezone)
-        else:
-            date = datetime.fromisoformat(date)
-            date = timezone.localize(date)
-    except ValueError as e:
-        raise HTTPException(status_code=HTTP_400_BAD_REQUEST)
-
-    client = await get_async_client()
-    db = client[MONGO_DB]
-    collection = db["routes"]
-    stations = db["stations"]
-    routes = []
-
-    start_of_day = timezone.localize(datetime.combine(date, datetime.min.time()))
-    end_of_day = timezone.localize(datetime.combine(date, datetime.max.time()))
-    query = {
-        "date": {
-            "$gte": start_of_day.isoformat(),
-            "$lt": end_of_day.isoformat()
-        }
-    }
-
-    async for route in collection.find(query):
-        route = get_route(route, station_from, station_to)
-        if route:
-            route["_id"] = str(route["_id"])
-            route = await set_coordinates(route, stations)
-            routes.append(route)
-    return routes
-
-
-def get_route(route, station_from: int, station_to: int) -> Dict or bool:
-    station_from_key = -1
-    station_to_key = -1
-    for key, station in enumerate(route['stations']):
-        if station['id'] == station_from:
-            station_from_key = key
-        if station['id'] == station_to:
-            station_to_key = key
-
-    if station_from_key == -1 or station_to_key == -1:
-        return False
-
-    if station_from_key >= station_to_key:
-        return False
-    route['stations'] = route['stations'][station_from_key:station_to_key + 1]
-
-    return route
-
-
-async def set_coordinates(route, stations):
-    for station in route['stations']:
-        db_station = await stations.find_one({"id": station['id']})
-        if db_station:
-            coords = db_station["coordinates"]
-            station["coordinates"] = coords
-
-    return route
 
 
 @router.get("/station/search/")
